c2586a2b1fa75f6811e22eff5296dda6c83b1c44
Peter Kona <void@vortex.local>
2014-06-14 22:28:04 +1200

    inline6

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require foundation
+
 //= require_tree .
 $(function() {
   $(document).foundation();
 });
+//$('#TjuvkikModal').foundation('reveal', 'open');
+//$('#ExempelobjektModal').foundation('reveal', 'open');
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,8 +25,8 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true  
-
+  config.assets.debug = true
+  config.serve_static_assets = false
     # config.action_mailer.smtp_settings = {
     #   :address   => "smtp.mandrillapp.com",
     #   :port      => 587,
